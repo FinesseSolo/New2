ef6abf4b6de8bc832e4e416bdcce0d3f4d7420dd
Hancock <bchancock.2017@mymail.jcsu.edu>
2018-01-24 14:36:13 -0500

    first

---
+++ Test2.java
@@ -0,0 +1,14 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 50;
+		while (i <= 350)
+		{
+			System.out.println("Number is " + i);
+			i = (i + 10);
+	}
+
+}
+}
